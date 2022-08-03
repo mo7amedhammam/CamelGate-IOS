77e487e929c8237c8f3fcbcff6d574a2ace326f4
mohamed hammam <mohamed.hammam.pro@gmail.com>
2022-08-03 11:19:31 +0200

    have a problem in soya request

--- Camel Gate/Authontication/Login/View/SignInView.swift
+++ Camel Gate/Authontication/Login/View/SignInView.swift
@@ -10,123 +10,128 @@ import SwiftUI
 struct SignInView: View {
     
     @StateObject var SignInVM = SignInViewModel()
-
+    
     @State var active = false
     @State var destination = AnyView(SignUpView())
-
+    
     var body: some View {
-            ZStack{
-                VStack{
-                    Image("signupheaderpng")
-                        .resizable()
-                        .padding(.top,-50)
-                        .frame(height:320)
+        ZStack{
+            VStack{
+                Image("signupheaderpng")
+                    .resizable()
+                    .padding(.top,-50)
+                    .frame(height:320)
+                
+                ScrollView{
+                    Image("LOGO")
+                        .padding(.horizontal,90)
                     
-                    ScrollView{
-                        Image("LOGO")
-                            .padding(.horizontal,90)
+                    Group{
                         
-                        Group{
-                            
-                            InputTextField(iconName: "phoneBlue",iconColor: Color("blueColor"), placeholder: "Enter_your_phone_number".localized(language), text: $SignInVM.phoneNumber)
-                                .keyboardType(.numberPad)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 8)
-                                        .stroke(.red, lineWidth:SignInVM.validations == .PhoneNumber ? 1:0))
-                                .onChange(of: SignInVM.phoneNumber){ newval in
-                                    SignInVM.phoneNumber =  String(newval.prefix(SignInVM.characterLimit))
-                                }
-
-                            if SignInVM.validations == .PhoneNumber{
-                                HStack{
-                                    Text(SignInVM.ValidationMessage.localized(language))
-                                        .foregroundColor(.red)
-                                        .font(Font.camelfonts.Reg14)
-                                    Spacer()
-                                }.padding(.horizontal)
+                        InputTextField(iconName: "phoneBlue",iconColor: Color("blueColor"), placeholder: "Enter_your_phone_number".localized(language), text: $SignInVM.phoneNumber)
+                            .keyboardType(.numberPad)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(.red, lineWidth:SignInVM.validations == .PhoneNumber ? 1:0))
+                            .onChange(of: SignInVM.phoneNumber){ newval in
+                                SignInVM.phoneNumber =  String(newval.prefix(SignInVM.characterLimit))
                             }
-                            
-
-                            SecureInputTextField("Enter_your_password".localized(language), text: $SignInVM.password,iconName:"lockBlue")
+                        
+                        if SignInVM.validations == .PhoneNumber{
+                            HStack{
+                                Text(SignInVM.ValidationMessage.localized(language))
+                                    .foregroundColor(.red)
+                                    .font(Font.camelfonts.Reg14)
+                                Spacer()
+                            }.padding(.horizontal)
                         }
-                        .font(Font.camelfonts.Reg16)
-                        .ignoresSafeArea(.keyboard)
                         
-                        HStack{
-                            Spacer()
-                            Text("Forgot_Password?".localized(language))
-                                .foregroundColor(.red)
-                                .font(Font.camelfonts.Reg14)
-
-                        }.padding(.horizontal)
                         
+                        SecureInputTextField("Enter_your_password".localized(language), text: $SignInVM.password,iconName:"lockBlue")
                     }
-                        
-                    Spacer()
-                }
-                .edgesIgnoringSafeArea(.bottom)
-                .onTapGesture(perform: {
-                    hideKeyboard()
-                })
-                
-    //            .padding(.horizontal,-30)
-                BottomSheetView(IsPresented: .constant(true), withcapsule: false, bluryBackground: false, forgroundColor: .clear, content: {
-                    VStack{
-    //                    Spacer()
-                        GradientButton(action: {
-                            print(SignInVM.phoneNumber)
-                            print(SignInVM.password)
-                            SignInVM.Login()
-                        }, Title: "Create_account".localized(language))
-                        
-                        HStack {
-                            Text("dont_have_an_Account? ".localized(language)).foregroundColor(.secondary)
-                            
-                            Button("Sign_Up".localized(language)) {
-                                print("goto signup")
-                                active = true
-                                destination = AnyView(SignUpView())
-                              }
-                            .font(.system(size: 13, weight: .bold))
-                            .foregroundColor(Color("blueColor"))
-                            
-                        }
-                        .padding(.top,-10)
-                    }
-                    .padding(.bottom,30)
-                    .background(
-                        Image("bottomBackimg")
-                            .resizable()
-                            .padding(.horizontal, -30)
-                            .padding(.bottom,-25)
-                    )
-
-                })
-                
-                
-            }
-            .navigationViewStyle(StackNavigationViewStyle())
-            
-            .overlay(content: {
-                VStack{
+                    .font(Font.camelfonts.Reg16)
+                    .ignoresSafeArea(.keyboard)
+                    
                     HStack{
-    //                    BackButtonView()
                         Spacer()
-                        Text("Sign_In".localized(language))
-                            .foregroundColor(.white)
-                            .font(Font.camelfonts.Med20)
-                           Spacer()
-    //                    Spacer().frame(width:50)
-                    }
-                    .padding(.horizontal)
-                    .padding(.top,40)
-                    Spacer()
+                        Text("Forgot_Password?".localized(language))
+                            .foregroundColor(.red)
+                            .font(Font.camelfonts.Reg14)
+                        
+                    }.padding(.horizontal)
+                    
                 }
+                
+                Spacer()
+            }
+            .edgesIgnoringSafeArea(.bottom)
+            .onTapGesture(perform: {
+                hideKeyboard()
             })
+            
+            //            .padding(.horizontal,-30)
+            BottomSheetView(IsPresented: .constant(true), withcapsule: false, bluryBackground: false, forgroundColor: .clear, content: {
+                VStack{
+                    //                    Spacer()
+                    GradientButton(action: {
+                        print(SignInVM.phoneNumber)
+                        print(SignInVM.password)
+                        SignInVM.Login()
+                    }, Title: "Create_account".localized(language))
+                    
+                    HStack {
+                        Text("dont_have_an_Account? ".localized(language)).foregroundColor(.secondary)
+                        
+                        Button("Sign_Up".localized(language)) {
+                            print("goto signup")
+                            active = true
+                            destination = AnyView(SignUpView())
+                        }
+                        .font(.system(size: 13, weight: .bold))
+                        .foregroundColor(Color("blueColor"))
+                        
+                    }
+                    .padding(.top,-10)
+                }
+                .padding(.bottom,30)
+                .background(
+                    Image("bottomBackimg")
+                        .resizable()
+                        .padding(.horizontal, -30)
+                        .padding(.bottom,-25)
+                )
+                
+            })
+            
+            // Alert with no internet connection
+            .alert(isPresented: $SignInVM.isAlert, content: {
+                Alert(title: Text(SignInVM.message), message: nil, dismissButton: Alert.Button.default(Text("OK".localized(language)), action: {
+                    SignInVM.isAlert = false
+                }))
+            })
+        }
+        .overlay(content: {
+            VStack{
+                HStack{
+                    //                    BackButtonView()
+                    Spacer()
+                    Text("Sign_In".localized(language))
+                        .foregroundColor(.white)
+                        .font(Font.camelfonts.Med20)
+                    Spacer()
+                    //                    Spacer().frame(width:50)
+                }
+                .padding(.horizontal)
+                .padding(.top,40)
+                Spacer()
+            }
+        })
+   
+        .navigationViewStyle(StackNavigationViewStyle())
         .navigationBarHidden(true)
-           
-            NavigationLink(destination: destination,isActive:$active , label: {
-            })
+        
+        NavigationLink(destination: destination,isActive:$active , label: {
+        })
     }
 }
 
--- Camel Gate/Authontication/Login/ViewModel/SignInViewModel.swift
+++ Camel Gate/Authontication/Login/ViewModel/SignInViewModel.swift
@@ -15,7 +15,7 @@ import PromiseKit
 class SignInViewModel: ObservableObject {
     
     let passthroughSubject = PassthroughSubject<String, Error>()
-//    let passthroughModelSubject = PassthroughSubject<BaseResponse<LoginModel>, Error>()
+    let passthroughModelSubject = PassthroughSubject<BaseResponse<LoginModel>, Error>()
     private let authServices = MoyaProvider<AuthServices>()
     private var cancellables: Set<AnyCancellable> = []
     let characterLimit: Int = 14
@@ -31,10 +31,10 @@ class SignInViewModel: ObservableObject {
                 validations = .PhoneNumber
                 self.ValidationMessage = "Enter_a_valid_Phone_number"
             }
-//            else if self.phoneNumber.isEmpty {
-//                validations = .PhoneNumber
-//                self.ValidationMessage = "*"
-//            }
+            //            else if self.phoneNumber.isEmpty {
+            //                validations = .PhoneNumber
+            //                self.ValidationMessage = "*"
+            //            }
             else if self.phoneNumber.count == characterLimit {
                 validations = .none
                 self.ValidationMessage = ""
@@ -51,65 +51,76 @@ class SignInViewModel: ObservableObject {
     //------- output
     @Published var validations: InvalidFields = .none
     @Published var ValidationMessage = ""
-//    @Published var inlineErrorPassword = ""
-//    @Published var publishedUserLogedInModel: LoginModel? = nil
+    @Published var publishedUserLogedInModel: LoginModel? = nil
     @Published var isLogedin = false
-
+    
     @Published var isLoading:Bool? = false
     @Published var isAlert = false
     @Published var activeAlert: ActiveAlert = .NetworkError
     @Published var message = ""
-
     
     @Published var destination = AnyView(TabBarView())
     init() {
-
-//        passthroughModelSubject.sink { (completion) in
-//        } receiveValue: { [self](modeldata) in
-//            publishedUserLogedInModel = modeldata.data
-//            if publishedUserLogedInModel?.ProfileStatus == 0 {
-//                destination = AnyView(PersonalDataView())
-//            }else if publishedUserLogedInModel?.ProfileStatus == 1{
-//                destination = AnyView(MedicalStateView())
-//
-//            }else if self.publishedUserLogedInModel?.ProfileStatus == 2{
-//                Helper.setUserData(Id: publishedUserLogedInModel?.Id ?? 0, PhoneNumber: publishedUserLogedInModel?.Phone ?? "", patientName: publishedUserLogedInModel?.Name ?? "" )
-//                Helper.setUserimage(userImage: URLs.BaseUrl+"\(publishedUserLogedInModel?.Image ?? "")")
-//                destination = AnyView(TabBarView())
-//            }
-//            Helper.setAccessToken(access_token: "Bearer " + "\(publishedUserLogedInModel?.Token ?? "")" )
-//
-//        }.store(in: &cancellables)
+        
+        passthroughModelSubject.sink { (completion) in
+        } receiveValue: { [self](modeldata) in
+            publishedUserLogedInModel = modeldata.data
+            if publishedUserLogedInModel?.statusId == 1 {
+                //                destination = AnyView(driverinfoView())
+                message =  "complete your prifile first"
+            }else if publishedUserLogedInModel?.statusId == 2{
+                destination = AnyView(TabBarView())
+                
+            }
+            //                Helper.setUserData(Id: publishedUserLogedInModel?.companyId ?? 0, PhoneNumber: publishedUserLogedInModel?.Phone ?? "", patientName: publishedUserLogedInModel?.Name ?? "" )
+            //                Helper.setUserimage(userImage: URLs.BaseUrl+"\(publishedUserLogedInModel?.Image ?? "")")
+            //                destination = AnyView(TabBarView())
+            
+            Helper.setAccessToken(access_token: "Bearer " + "\(publishedUserLogedInModel?.token ?? "")" )
+            //
+        }.store(in: &cancellables)
         
     }
+    
+    // MARK: - API Services
+    func Login(){
+        let params : [String : Any] =
+        [
+            "mobile"                       : phoneNumber ,
+            "password"                    : password
+        ]
+        firstly { () -> Promise<Any> in
+            //              self.startProgress()
+            isLoading = true
+            return BGServicesManager.CallApi(self.authServices,AuthServices.Login(parameters: params))
+        }.done({ [self] response in
+            let result = response as! Response
+            guard BGNetworkHelper.validateResponse(response: result) else{return}
+            let data : BaseResponse<LoginModel> = try BGDecoder.decode(data: result.data)
+            if data.messageCode == 200{
+                DispatchQueue.main.async {
+                    passthroughModelSubject.send(data)
+                    isLogedin = true
+                }
+            }else if data.messageCode == 400 {
+//                print(data.message ?? "Bad Request")
+                message = data.message ?? "Bad Request"
+                isAlert = true
+            }
+            message = data.message ?? "Bad Request"
+            isAlert = true
 
-  // MARK: - API Services
-      func Login(){
-          let params : [String : Any] =
-          [
-              "mobile"                       : phoneNumber ,
-              "password"                    : password
-          ]
-          firstly { () -> Promise<Any> in
-//              self.startProgress()
-              return BGServicesManager.CallApi(self.authServices,AuthServices.Login(parameters: params))
-          }.done({ response in
-              let result = response as! Response
-              guard BGNetworkHelper.validateResponse(response: result) else{return}
-              let data : BaseResponse<LoginModel> = try BGDecoder.decode(data: result.data)
-              self.message = data.message ?? ""
-             print(data.message ?? "")
-              print(data)
-          }).ensure {
-//              self.stopProgress()
-          }.catch { (error) in
-              print(error)
-              print(error.localizedDescription)
-
-
-//              BGAlertPresenter.displayToast(title: "" , message: "\(error)", type: .error)
-          }
-      }
+        }).ensure { [self] in
+            isLoading = false
+            //              self.stopProgress()
+        }.catch { [self] (error) in
+//            print(error)
+//            print(error.localizedDescription)
+            isAlert = true
+            message = "\(error)"
+            //              BGAlertPresenter.displayToast(title: "" , message: "\(error)", type: .error)
+        }
+    }
 }
 
 
